35fb2d0e9535bb0af448dfbe7440c0ad7bebea1b
Alexandre <alexandresaad@icloud.com>
2021-11-06 14:02:37 +0100

    Commit

--- Tests/SwiftUIExtensionsTests/Color_Tests.swift
+++ Tests/SwiftUIExtensionsTests/Color_Tests.swift
@@ -1,11 +1,27 @@
 // Color_Tests.swift
-// ArithmeticsTests
+// SwiftUIExtensionsTests
 //
 // Copyright © 2021 Alexandre H. Saad
 // Licensed under Apache License v2.0 with Runtime Library Exception
 //
 
+import SwiftUI
 import XCTest
 @testable import SwiftUIExtensions
 
-final class Color_Tests: XCTestCase {}
+final class Color_Tests: XCTestCase {
+	func test_InitializedWithHex_Succeeds() {
+		// Given
+		let red: Color = .init(red: 1, green: 0, blue: 0)
+		let hex3: Color? = .init(hex: "F00")
+		let hex3H: Color? = .init(hex: "#F00")
+		let hex6: Color? = .init(hex: "FF0000")
+		let hex6H: Color? = .init(hex: "#FF0000")
+		
+		// Then
+		XCTAssertEqual(red, hex3)
+		XCTAssertEqual(red, hex3H)
+		XCTAssertEqual(red, hex6)
+		XCTAssertEqual(red, hex6H)
+	}
+}

eeaabe1570e0cb291199df777354abfdcc39e381
Alexandre <alexandresaad@icloud.com>
2021-11-22 09:58:04 +0100

    Commit

--- Sources/SwiftUIX/Objects/Flowers/Flower/Flower.swift
+++ Sources/SwiftUIX/Objects/Flowers/Flower/Flower.swift
@@ -104,7 +104,7 @@ extension Flower: View {
 			// The rotation animation.
 			.rotationEffect(.degrees(self.isMinimized ? -90 : 0))
 			// The scale animation.
-			.scaleEffect(self.isMinimized ? 0.2 : 1)
+			.scaleEffect(self.isMinimized ? 0.25 : 1)
 			// The top position of the first petal.
 			.rotationEffect(.degrees(-90))
 			// The clockwise animation on the entry of petals.

b922960f78ef886e440cb686abe1ca2bb10461d0
Alexandre <alexandresaad@icloud.com>
2022-03-25 09:20:34 +0100

    Commit

---
+++ Sources/SwiftUIX/Environments/EnvironmentValues/ChartStyle/EnvironmentValues+.swift
@@ -0,0 +1,27 @@
+// EnvironmentValues+.swift
+// SwiftUIX
+//
+// Copyright © 2021 Alexandre H. Saad
+//
+
+import SwiftUI
+
+extension EnvironmentValues {
+	/// A boolean value indicating whether this environment is running in a preview.
+	public var isRunningInPreview: Bool {
+		#if DEBUG
+		return ProcessInfo.processInfo.environment["XCODE_RUNNING_FOR_PREVIEWS"] == "1"
+		#else
+		return false
+		#endif
+	}
+	
+	/// A boolean value indicating whether this environment is running in a simulator.
+	public var isRunningInSimulator: Bool {
+		#if targetEnvironment(simulator)
+		return true
+		#else
+		return false
+		#endif
+	}
+}
